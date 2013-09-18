608d8206a000baeb0cb44b364d14d092014419b9
Superdev0624 <superbaymax007@gmail.com>
2013-09-18 09:54:20 +0800

    Add async parsing option.

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,7 +54,22 @@ module.exports = function(grunt) {
     },
     jshint: {
       all: ['<%= concat.dist.dest %>'],
-      options: grunt.file.readJSON('./.jshintrc')
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+      }
     }
   });
 
